2c55bc705d9bf8656144465b3d7c2415ec88e8b4
sophie laluashvili <sophielaluashvili@gmail.com>
2023-07-23 21:15:29 +0400

    Models

---
+++ RunGroupWebApp/Models/Club.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RunGroupWebApp.Models
+{
+    public class Club
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Descriprion { get; set; }
+        public string Image { get; set; }
+        [ForeignKey("Address")]
+        public int MyProperty { get; set; }
+
+    }
+}
